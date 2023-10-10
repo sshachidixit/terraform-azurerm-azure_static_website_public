1c5c58e1ed42ae48a668b0d001d0ff61d355eb4c
sshachidixit <117880753+sshachidixit@users.noreply.github.com>
2023-10-10 21:14:31 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -6,10 +6,10 @@ terraform {
     }*/
   }
   /*backend "azurerm" {
-    resource_group_name = "terraform-storage-rg"
-    storage_account_name = "tfazurestateshachi"
-    container_name = "tfstatefiles"
-    key = "terraform.tfstate"
+    resource_group_name = "****"
+    storage_account_name = "******"
+    container_name = "*****"
+    key = "******"
     
   }*/
 }
